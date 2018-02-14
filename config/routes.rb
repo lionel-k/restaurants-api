d89e23a2b34ee58be545373996aab79e5884b9aa
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-02-14 18:23:04 +0100

    keep routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # root to: 'pages#home'
+  devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :restaurants, only: [ :index, :show ]
